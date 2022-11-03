ecfa119960e14de18fc1ab0b4d2af14390f82823
gmavro13 <80090327+gmavro13@users.noreply.github.com>
2022-11-03 15:13:31 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    myHeading.textContent = `Weed maaan, ${myName}`;
   }
 }
 if (!localStorage.getItem("name")) {
